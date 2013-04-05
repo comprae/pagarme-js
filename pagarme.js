2bd4ccbc1d97b5c6b8032e2472dec2f10eb72666
Pedro Franceschi <pedrohfranceschi@gmail.com>
2013-04-05 20:15:56 -0300

    Move public key definition to the top of the file.

--- pagarme.js
+++ pagarme.js
@@ -1,3 +1,15 @@
+var pagarmePublicKeyString = "-----BEGIN PUBLIC KEY-----\
+					   MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3+L/5UB2xen6FnMjpQo7\
+					   ZOfoeYd4DFW7OtdQKqGRXdI4JYctZlbBbU5OoNH5clIZxUYi9sG15g8iieYxc77E\
+					   ZLSJFx+H7NuzeOBBQuCT7liYFiCm7LH0iyqPnFYQOoYHvibB+zVEm9/H5+qE3i1i\
+					   ZBuvT/5v0o4iemVLNzIGJs360atEHHnKjfqhpobzXQ0+BdqYu2VWEsCzhFRDt+yp\
+					   O8ZBcI+Fd0+SZYjOw0tyE1NousG5TvnST1SApucb44bD2PEIQ1RzTNRzhc+NVoTu\
+					   LYvotLGCm/SxJUkX/0RIMw6VJq1gXAGUdKE/W1ScFkGBbtyoqWvtlvvg9gQdmFLC\
+					   XwIDAQAB\
+					   -----END PUBLIC KEY-----";
+// Our public key goes on top of the file to be easier to dynamically 
+// update it from Chimera using the public key saved in its filesystem.
+
 // #### Start of jsbn.js ####
 
 
@@ -1843,20 +1855,7 @@ PagarMeCreditCard.prototype.generateHash = function() {
 	var stringifiedParameters = this.stringifyParameters();
 	console.log(stringifiedParameters);
 
-	// Our public key
-	var publicKeyString = "-----BEGIN PUBLIC KEY-----\
-		MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3+L/5UB2xen6FnMjpQo7\
-		ZOfoeYd4DFW7OtdQKqGRXdI4JYctZlbBbU5OoNH5clIZxUYi9sG15g8iieYxc77E\
-		ZLSJFx+H7NuzeOBBQuCT7liYFiCm7LH0iyqPnFYQOoYHvibB+zVEm9/H5+qE3i1i\
-		ZBuvT/5v0o4iemVLNzIGJs360atEHHnKjfqhpobzXQ0+BdqYu2VWEsCzhFRDt+yp\
-		O8ZBcI+Fd0+SZYjOw0tyE1NousG5TvnST1SApucb44bD2PEIQ1RzTNRzhc+NVoTu\
-		LYvotLGCm/SxJUkX/0RIMw6VJq1gXAGUdKE/W1ScFkGBbtyoqWvtlvvg9gQdmFLC\
-		XwIDAQAB\
-		-----END PUBLIC KEY-----";
-
-	console.log(publicKey);
-
-	var publicKey = RSA.getPublicKey(publicKeyString);
+	var publicKey = RSA.getPublicKey(pagarmePublicKeyString);
 	var encryptedString = RSA.encrypt(stringifiedParameters, publicKey);
 
 	console.log(encryptedString);
