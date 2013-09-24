f9ca6041e5ce4f9348033306a276a464736042c4
Pedro Franceschi <pedrohfranceschi@gmail.com>
2013-09-24 12:23:40 -0300

    Use Pagar.me's URLs to mask real profiling URL.

--- pagarme.js
+++ pagarme.js
@@ -1871,12 +1871,12 @@ PagarMe.enableAntifraudProfiling = function() {
 
     PagarMe.sessionId = randomKey;
 
-	var profilingTags = "<p style=\"background:url(https://h.online-metrix.net/fp/clear.png?org_id=k6dvnkdk&session_id=" + PagarMe.sessionId + "&m=1)\"> </p>\
-	<img src=\"https://h.online-metrix.net/fp/clear.png?org_id=k6dvnkdk&amp;session_id=" + PagarMe.sessionId + "&m=2\" alt=\"\" >\
-	<script src=\"https://h.online-metrix.net/fp/check.js?org_id=k6dvnkdk&amp;session_id=" + PagarMe.sessionId + "\" type=\"text/javascript\">\
+	var profilingTags = "<p style=\"background:url(https://api.pagar.me/1/profiling/fp/clear.png?org_id=k6dvnkdk&session_id=" + PagarMe.sessionId + "&m=1)\"> </p>\
+	<img src=\"https://api.pagar.me/1/profiling/fp/clear.png?org_id=k6dvnkdk&amp;session_id=" + PagarMe.sessionId + "&m=2\" alt=\"\" >\
+	<script src=\"https://api.pagar.me/1/profiling/fp/check.js?org_id=k6dvnkdk&amp;session_id=" + PagarMe.sessionId + "\" type=\"text/javascript\">\
 	</script>\
-	<object type=\"application/x-shockwave-flash\" data=\"https://h.online-metrix.net/fp/fp.swf?org_id=k6dvnkdk&session_id=" + PagarMe.sessionId + "\" width=\"1\" height=\"1\" id=\"obj_id\">\
-	<param name=\"movie\" value=\"https://h.online-metrix.net/fp/fp.swf?org_id=k6dvnkdk&session_id=" + PagarMe.sessionId + "\" /> </object>";
+	<object type=\"application/x-shockwave-flash\" data=\"https://api.pagar.me/1/profiling/fp/fp.swf?org_id=k6dvnkdk&session_id=" + PagarMe.sessionId + "\" width=\"1\" height=\"1\" id=\"obj_id\">\
+	<param name=\"movie\" value=\"https://api.pagar.me/1/profiling/fp/fp.swf?org_id=k6dvnkdk&session_id=" + PagarMe.sessionId + "\" /> </object>";
 
 	document.getElementsByTagName('body')[0].innerHTML += profilingTags;
 }
